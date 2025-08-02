cf78930538d3c3d9ea19f3de7b95b42de79de5d8
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2025-08-02 22:19:08 +0800

    feat: schema

--- apps/web/src/components/schema-form/index.tsx
+++ apps/web/src/components/schema-form/index.tsx
@@ -1,10 +1,30 @@
 import type { ComponentProps } from 'react'
+import type { FormConfig } from './types'
+import { Form, FormField, FormItem } from '@ago/ui/basic/form.tsx'
+import { useForm } from 'react-hook-form'
 
 interface SchemaFormProps extends ComponentProps<'form'> {
+  config: FormConfig
 }
 
 export function SchemaForm(props: SchemaFormProps) {
+  const { fields } = props.config
+
+  const form = useForm({})
+
   return (
-    <form {...props}></form>
+    <Form {...form}>
+      {fields.map(config => (
+        <FormField
+          control={form.control}
+          key={config.name}
+          name={config.name}
+          defaultValue={config.defaultValue}
+          render={({ field }) => (
+            <FormItem>{field.name}</FormItem>
+          )}
+        />
+      ))}
+    </Form>
   )
 }
---
+++ apps/web/src/components/schema-form/types.ts
@@ -0,0 +1,22 @@
+export type FieldType = 'text' | 'number' | 'email' | 'select' | 'checkbox'
+
+export interface FieldConfig<ValueType = any> {
+  name: string
+  label: string
+  type: FieldType
+  defaultValue?: ValueType
+  required?: boolean
+  minLength?: number
+  maxLength?: number
+  min?: number
+  max?: number
+  pattern?: RegExp
+  options?: Array<{ label: string, value: ValueType }>
+}
+
+export interface FormConfig {
+  fields: FieldConfig[]
+  endpoint: string
+  method?: 'POST' | 'PUT' | 'PATCH'
+  headers?: Record<string, string>
+}
